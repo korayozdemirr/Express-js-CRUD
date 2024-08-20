ae5bc5c56ba49164228140557b5044a0bdbdd3c0
Koray Özdemir <korayozdemir@192.168.1.101>
2024-08-21 00:41:05 +0300

    users register

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const productsRouter = require("./routes/products")
+const productsRouter = require("./routes/products");
+const userRouter = require("./routes/users");
 
 /* Parser */
-app.use(express.json())
+app.use(express.json());
 /* Routes */
-app.use("/products", productsRouter)
-
+app.use("/products", productsRouter);
+app.use("/user", userRouter);
 /*Mongo db connection*/
-mongoose.connect("mongodb+srv://admin:koray123@cluster0.yq5lq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-    console.log("Connected to database")
-})
-.catch((error)=>{
-    console.log(error)
-})
+mongoose
+  .connect(
+    "mongodb+srv://admin:koray123@cluster0.yq5lq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+  res.send("Hello world");
+});
 
 app.listen(5001, () => {
-    console.log("Server is Running on PORT 5001");
+  console.log("Server is Running on PORT 5001");
 });
 
 //cvS5MpJ6O1Ws8zgG
